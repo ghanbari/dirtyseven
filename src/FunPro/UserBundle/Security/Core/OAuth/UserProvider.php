3b2e472a7a854c8cf049f1d8d908cbc622a10a30
Ghanbari <ghanbari.daniel@gmail.com>
2016-12-15 11:34:13 +0330

    oauth

--- src/FunPro/UserBundle/Security/Core/OAuth/UserProvider.php
+++ src/FunPro/UserBundle/Security/Core/OAuth/UserProvider.php
@@ -42,7 +42,7 @@ class UserProvider extends BaseFOSUBProvider
             $user = $this->userManager->createUser();
             $user->setUsername($response->getUsername());
             $user->setEmail($response->getEmail());
-            $user->setPassword($response->getUsername());
+            $user->setPlainPassword($response->getUsername());
             $user->setEnabled(true);
             $this->userManager->updateUser($user);
 
